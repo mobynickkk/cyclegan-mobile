2fc089e546536264d76791a6ee49feb53565dc69
mobynick <mobynick365@gmail.com>
2021-01-31 21:54:34 +0300

    Added image size customization

--- utils/dataset.py
+++ utils/dataset.py
@@ -8,17 +8,17 @@ from pathlib import Path
 class ImgDataset(Dataset):
     """Dataset for CycleGAN"""
 
-    def __init__(self, files_a_dir, files_b_dir):
+    def __init__(self, files_a_dir, files_b_dir, img_size=256):
         super().__init__()
         self.files_a = tuple(Path(files_a_dir).rglob('*.jpg'))
         self.files_b = tuple(Path(files_b_dir).rglob('*.jpg'))
         self.length = min(len(self.files_a), len(self.files_b))
+        self.img_size = img_size
 
     def __len__(self):
         return self.length
 
-    @staticmethod
-    def image_loader(img_a, img_b):
+    def image_loader(self, img_a, img_b):
         if not torch.cuda.is_available():
             raise BaseException('GPU is not available')
         device = torch.device('cuda')
@@ -27,8 +27,8 @@ class ImgDataset(Dataset):
         img_b = Image.open(img_b)
         img_b.load()
         loader = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(256),
+            transforms.Resize(self.img_size),
+            transforms.CenterCrop(self.img_size),
             transforms.ToTensor()
         ])
         img_a = loader(img_a).to(device)
@@ -43,24 +43,24 @@ class ImgDataset(Dataset):
 class ShiftDataset(Dataset):
     """Dataset for CycleGAN with different pairs on each epoch"""
 
-    def __init__(self, files_dir):
+    def __init__(self, files_dir, img_size=256):
         super().__init__()
         self.files = tuple(Path(files_dir).rglob('*.jpg'))
         self.length = len(self.files)
+        self.img_size = img_size
 
     def __len__(self):
         return self.length
 
-    @staticmethod
-    def image_loader(img_a):
+    def image_loader(self, img_a):
         if not torch.cuda.is_available():
             raise BaseException('GPU is not available')
         device = torch.device('cuda')
         img_a = Image.open(img_a)
         img_a.load()
         loader = transforms.Compose([
-            transforms.Resize(128),
-            transforms.CenterCrop(128),
+            transforms.Resize(self.img_size),
+            transforms.CenterCrop(self.img_size),
             transforms.ToTensor()
         ])
         img_a = loader(img_a).to(device)
