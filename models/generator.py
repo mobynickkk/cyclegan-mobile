65ccc48cef0a141a6e100d339f66356d432be9fd
mobynick <mobynick365@gmail.com>
2021-04-24 20:45:33 +0300

    Reduced model size

--- models/generator.py
+++ models/generator.py
@@ -6,11 +6,11 @@ class Generator(nn.Module):
 
     def __init__(self):
         super(Generator, self).__init__()
-        self.e0 = TransformerBlock(3, 16, 3)
+        self.e0 = TransformerBlock(3, 8, 3)
         self.pool0 = nn.MaxPool2d(2, return_indices=True)
-        self.e1 = TransformerBlock(16, 16)
+        self.e1 = TransformerBlock(8, 12, 4)
         self.pool1 = nn.MaxPool2d(2, return_indices=True)
-        self.e2 = TransformerBlock(16, 16)
+        self.e2 = TransformerBlock(12, 16, 4)
         self.pool2 = nn.MaxPool2d(2, return_indices=True)
         self.e3 = TransformerBlock(16, 16)
         self.pool3 = nn.MaxPool2d(2, return_indices=True)
@@ -21,15 +21,15 @@ class Generator(nn.Module):
 
         self.unpool0 = nn.MaxUnpool2d(2)
 
-        self.d1 = TransformerBlock(16, 16)
+        self.d1 = TransformerBlock(16, 12, 4)
 
         self.unpool1 = nn.MaxUnpool2d(2)
 
-        self.d2 = TransformerBlock(16, 16)
+        self.d2 = TransformerBlock(12, 8, 4)
 
         self.unpool2 = nn.MaxUnpool2d(2)
 
-        self.d3 = TransformerBlock(16, 3)
+        self.d3 = TransformerBlock(8, 3, 4)
 
         self.unpool3 = nn.MaxUnpool2d(2)
 
